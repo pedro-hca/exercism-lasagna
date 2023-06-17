d9b3e1b43d00effe224b4cf9a4f8e227b8d8c6fd
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-17 20:43:02 -0300

    final commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lasagna
+{
+    class Lasagna
+    {
+        public static void Main(string[] args)
+        {
+            Lasagna lasagna = new Lasagna();
+
+            int expectedMinutesInOven = lasagna.ExpectedMinutesInOven();
+            Console.WriteLine(expectedMinutesInOven);
+            // Saída: 40
+
+            int remainingMinutesInOven = lasagna.RemainingMinutesInOven(20);
+            Console.WriteLine(remainingMinutesInOven);
+            // Saída: 20 (40 - 20)
+
+            int preparationTime = lasagna.PreparationTimeInMinutes(3);
+            Console.WriteLine(preparationTime);
+            // Saída: 6 (3 * 2)
+
+            int elapsedTime = lasagna.ElapsedTimeInMinutes(3, 20);
+            Console.WriteLine(elapsedTime);
+            // Saída: 26 (20 + 6)
+        }
+
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+
+        public int RemainingMinutesInOven(int minutesInOven)
+        {
+            return ExpectedMinutesInOven() - minutesInOven;
+        }
+
+        public int PreparationTimeInMinutes(int layers)
+        {
+            return layers * 2;
+        }
+
+        public int ElapsedTimeInMinutes(int layers, int minutesInOven)
+        {
+            return minutesInOven + PreparationTimeInMinutes(layers);
+        }
+
+    }
+
+
+}
+
+
